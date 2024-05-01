aa36f47a478c210cf837430089962ec27ccaeed2
Mustafa Kabakci <mustafakabakci4@gmail.com>
2024-05-01 10:42:29 -0400

    Added API calls to component and updated templates.

--- src/app/sales/next-best-action/next-best-action.component.ts
+++ src/app/sales/next-best-action/next-best-action.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
+
+interface NextBestAction {
+  content: string
+}
 
 @Component({
   selector: 'app-next-best-action',
@@ -8,7 +13,22 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
   styleUrls: ['./next-best-action.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NextBestActionComponent {
-  @Input()
-  similarArticle: any;
+export class NextBestActionComponent implements OnInit {
+  public nextBestAction: String[] = []
+
+  constructor(
+    private http: HttpClient,
+    private cd: ChangeDetectorRef
+  ) { }
+
+  ngOnInit(): void { }
+
+  getNextBestAction() {
+    const url = "http://localhost:5000/next_best_action";
+
+    this.http.post<NextBestAction>(url, {}).subscribe((response => {
+      this.nextBestAction = [response.content]
+      this.cd.markForCheck();
+    }));
+  }
 }
--- src/app/sales/summary/summary.component.ts
+++ src/app/sales/summary/summary.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
+
+interface Summary {
+  content: string
+}
 
 @Component({
   selector: 'app-summary',
@@ -8,7 +13,22 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
   styleUrls: ['./summary.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SummaryComponent {
-  @Input()
-  summary: any;
+export class SummaryComponent implements OnInit {
+  public summaries: string[] = [];
+
+  constructor(
+    private http: HttpClient,
+    private cd: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {}
+
+  getSummaries() {
+    const url = "http://localhost:5000/summary_rollup";
+
+    this.http.post<Summary>(url, {}).subscribe((response => {
+      this.summaries = [response.content];
+      this.cd.markForCheck();
+    }));
+  }
 }
